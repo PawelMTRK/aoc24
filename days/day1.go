91de09d2c59302a811c25566d57d18a5398064f1
PawelMTRK <pawelmat@onet.pl>
2024-12-08 16:51:59 +0100

    Dividing it all into smaller steps

--- days/day1.go
+++ days/day1.go
@@ -2,10 +2,39 @@ package days
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+type HistorianList struct {
+	list []int
+}
+
+func (l *HistorianList) Append(v string) {
+	n, _ := strconv.Atoi(v)
+	l.list = append(l.list, n)
+}
+
+func (l *HistorianList) Load(slice []string) {
+	l.list = make([]int, 0)
+	for _, v := range slice {
+		n, _ := strconv.Atoi(v)
+		l.list = append(l.list, n)
+	}
+}
+
+func (l *HistorianList) GetDifference(l2 *HistorianList) int {
+	slices.Sort(l.list)
+	slices.Sort(l2.list)
+	diff := 0
+	for i, _ := range l.list {
+		diff += int(math.Abs(float64(l.list[i] - l2.list[i])))
+	}
+	return diff
+}
 
 func Day1() {
 	res, err := os.ReadFile("input.txt")
@@ -15,15 +44,17 @@ func Day1() {
 	stringRes := string(res)
 	sliceNums := strings.Fields(stringRes)
 
-	list1 := make([]int, 0)
-	list2 := make([]int, 0)
+	list1 := HistorianList{}
+	list2 := HistorianList{}
+
 	for i, e := range sliceNums {
-		n, _ := strconv.Atoi(e)
 		if i%2 == 0 {
-			list1 = append(list1, n)
+			list1.Append(e)
 		} else {
-			list2 = append(list2, n)
+			list2.Append(e)
 		}
 	}
-	fmt.Println(list1, list2)
+
+	diff := list1.GetDifference(&list2)
+	fmt.Println("The difference is:", diff)
 }
