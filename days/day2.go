7e325f7aaa860e16f0ed54747f8d46a02b2fe76b
PawelMTRK <pawelmat@onet.pl>
2024-12-08 17:36:29 +0100

    Day2 Add direction detection

--- days/day2.go
+++ days/day2.go
@@ -22,19 +22,33 @@ func NewReport(line string) Report {
 
 func (r *Report) IsSafe() bool {
 	last := r.levels[0]
+	direction := 0
 	for i, l := range r.levels {
 		// HACK skip comparing first element with itself
 		if i == 0 {
 			continue
 		}
+
 		diff := l - last
-		// apparently there is no int abs function :c
+
+		if direction == -1 && 0 < diff {
+			return false
+		} else if direction == 1 && diff < 0 {
+			return false
+		}
+
+		// no int abs function workaround
+		// and direction detection
 		if diff < 0 {
 			diff = -diff
+			direction = -1
+		} else if diff > 0 {
+			direction = 1
 		}
 		if 3 < diff || diff < 1 {
 			return false
 		}
+
 		last = l
 	}
 	return true
