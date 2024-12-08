13a874d05d6f91ac76e1b43884e826f4fae02827
PawelMTRK <pawelmat@onet.pl>
2024-12-08 17:44:23 +0100

    Automate displaying separators when calling day functions

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"fmt"
 )
 
+func RunDay(n int, dayfunc func()) {
+	fmt.Println("-*", n)
+	dayfunc()
+}
+
 func main() {
 	fmt.Println("-_* AOC 2024 *_-")
-	days.Day1()
+	RunDay(1, days.Day1)
+	RunDay(2, days.Day2)
 }
