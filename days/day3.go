36320c560e4ec9e19d26d192f13c3b38bca548de
PawelMTRK <pawelmat@onet.pl>
2024-12-10 00:19:30 +0100

    Day3 Part 2 Implement toggling of the multiplication

--- days/day3.go
+++ days/day3.go
@@ -1,6 +1,7 @@
 package days
 
 import (
+	"aoc24/util"
 	"fmt"
 	"os"
 	"strconv"
@@ -12,55 +13,41 @@ type Puter struct {
 	mulArgs   [][2]int
 }
 
-func IndexN(s string, substr string, n int) int {
-	i, last := 0, 0
-	for range n {
-		i = strings.Index(s[last:], substr)
-		if i == -1 {
-			return -1
+func NewPuter(s string) Puter {
+	muls := util.GetIndexes(s, "mul")
+	enabled := true
+	memoryBlocks := make([]string, 0)
+	var block string
+	for i := range muls {
+		if i == len(muls)-1 {
+			block = s[muls[i]:]
+		} else {
+			block = s[muls[i]:muls[i+1]]
 		}
-		last += i + 1
+		if enabled {
+			memoryBlocks = append(memoryBlocks, block)
+		}
+		util.ToggleBool(&enabled, strings.Contains(block, "do()"), strings.Contains(block, "don't()"))
 	}
-	// last variable contains the last index of the substring + 1
-	// so we decrement it
-	i = last - 1
-	if i < 0 {
-		i = 0
-	}
-	return i
-}
-
-func GetIndexes(s string, substr string) []int {
-	count := strings.Count(s, substr)
-	indexes := make([]int, count)
-	fmt.Println(count)
-	if count == 0 {
-		return nil
-	}
-	for i := range count {
-		indexes[i] = IndexN(s, substr, i+1)
-	}
-	return indexes
-}
-
-func NewPuter(v string) Puter {
-	memoryBlocks := strings.Split(v, "mul")
-	return Puter{mulBlocks: memoryBlocks[1:]}
+	return Puter{mulBlocks: memoryBlocks}
 }
 
 func (p *Puter) ParseBlocks() {
 	for _, v := range p.mulBlocks {
-		// fmt.Println("Parsing...", v)
+		validStart := strings.Contains(v, "mul(")
+		if !validStart {
+			continue
+		}
 		closeParenI := strings.Index(v, ")")
 		if closeParenI == -1 {
 			continue
 		}
-		rawArgs := v[1:closeParenI]
+		// start before "mul("
+		rawArgs := v[4:closeParenI]
 		commaI := strings.Index(rawArgs, ",")
 		if commaI == -1 {
 			continue
 		}
-		// fmt.Println(rawArgs[:commaI], rawArgs[commaI+1:])
 		n1, _ := strconv.Atoi(rawArgs[:commaI])
 		n2, _ := strconv.Atoi(rawArgs[commaI+1:])
 		p.mulArgs = append(p.mulArgs, [2]int{n1, n2})
@@ -70,7 +57,6 @@ func (p *Puter) ParseBlocks() {
 func (p *Puter) Calculate() int {
 	total := 0
 	for _, a := range p.mulArgs {
-		// fmt.Println(a[0], "*", a[1])
 		total += (a[0] * a[1])
 	}
 	return total
