16dc22b8a1d09b65de8597cc93059d349949ced5
PawelMTRK <pawelmat@onet.pl>
2024-12-11 16:00:38 +0100

    Day4 Part 2 Find X shaped MAS string

--- days/day4.go
+++ days/day4.go
@@ -1,6 +1,7 @@
 package days
 
 import (
+	"aoc24/util"
 	"fmt"
 	"os"
 	"strings"
@@ -17,11 +18,11 @@ type WordSearch struct {
 func NewWordSearch(s string) *WordSearch {
 	lines := strings.Split(s, "\n")
 	ws := WordSearch{puzzle: lines, total: 0}
-	ws.wg.Add(4)
 	return &ws
 }
 
 func (w *WordSearch) isXMASorSAMX(x, m, a, s byte) bool {
+	// X - 88, 77 - M, 64 - A, 83 - S
 	if x == 88 && m == 77 && a == 65 && s == 83 {
 		return true
 	} else if x == 83 && m == 65 && a == 77 && s == 88 {
@@ -30,7 +31,24 @@ func (w *WordSearch) isXMASorSAMX(x, m, a, s byte) bool {
 	return false
 }
 
-func (w *WordSearch) FindByDirection(dir string) {
+func (w *WordSearch) isExMAS(row1, row2, row3 string) bool {
+	if row2[1] != 65 {
+		return false
+	}
+	// 77 - M, 83 - S
+	// find any of the 4 combinations
+	// M M  S S  M S  S M
+	//  A    A    A    A
+	// S S  M M  M S  S M
+	return util.Any(
+		util.Compare3(row1[0], row1[2], 77) && util.Compare3(row3[0], row3[2], 83),
+		util.Compare3(row1[0], row1[2], 83) && util.Compare3(row3[0], row3[2], 77),
+		util.Compare3(row1[0], row3[0], 83) && util.Compare3(row1[2], row3[2], 77),
+		util.Compare3(row1[0], row3[0], 77) && util.Compare3(row1[2], row3[2], 83),
+	)
+}
+
+func (w *WordSearch) FindXMASByDirection(dir string) {
 	switch dir {
 	case "-":
 		for r := range len(w.puzzle) {
@@ -76,14 +94,28 @@ func (w *WordSearch) FindByDirection(dir string) {
 	w.wg.Done()
 }
 
+func (w *WordSearch) FindExMAS() {
+	for r := range len(w.puzzle) - 2 {
+		for c := range len(w.puzzle[r]) - 2 {
+			if w.isExMAS(w.puzzle[r][c:c+3], w.puzzle[r+1][c:c+3], w.puzzle[r+2][c:c+3]) {
+				w.total++
+			}
+		}
+	}
+}
+
 func Day4() {
 	res, _ := os.ReadFile("input4.txt")
 	stringRes := string(res)
 	wordSearch := NewWordSearch(stringRes)
-	go wordSearch.FindByDirection("-")
-	go wordSearch.FindByDirection("|")
-	go wordSearch.FindByDirection("\\")
-	go wordSearch.FindByDirection("/")
+	wordSearch.wg.Add(4)
+	go wordSearch.FindXMASByDirection("-")
+	go wordSearch.FindXMASByDirection("|")
+	go wordSearch.FindXMASByDirection("\\")
+	go wordSearch.FindXMASByDirection("/")
 	wordSearch.wg.Wait()
 	fmt.Println("Total number of XMAS:", wordSearch.total)
+	wordSearch.total = 0
+	wordSearch.FindExMAS()
+	fmt.Println("Total number of X-MAS:", wordSearch.total)
 }
